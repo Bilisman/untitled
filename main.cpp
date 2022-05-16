e55d385ed3c559128224b7801013cabd78644aba
Bilisman <105249418+Bilisman@users.noreply.github.com>
2022-05-16 10:51:07 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World" << std::endl;
     std::cout << "Hello, Word" << std::endl;
+     std::cout << "Hello, Wrd" << std::endl;
     return 0;
 }
